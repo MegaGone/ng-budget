1aab7aa9dbdf9b0ef646734aa976a05f3d10ecc6
Javier Martínez <jmartinez@develsystems.com>
2022-08-06 14:12:24 -0600

    Start to work in login component

--- BudgetApp.Frontend/src/app/auth/login/login.component.ts
+++ BudgetApp.Frontend/src/app/auth/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector      : 'app-login',
+  templateUrl   : './login.component.html',
+  styleUrls     : ['./login.component.scss'],
+  encapsulation : ViewEncapsulation.None,
+  animations    : fuseAnimations
 })
 export class LoginComponent implements OnInit {
 
