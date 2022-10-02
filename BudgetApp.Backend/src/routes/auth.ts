1a19884a3dd73d25aa872f75d6a955806605acc0
Javier Martínez <jmartinez@develsystems.com>
2022-10-02 17:30:15 -0600

    [ADDED] Documentation to login

--- BudgetApp.Backend/src/routes/auth.ts
+++ BudgetApp.Backend/src/routes/auth.ts
@@ -5,6 +5,24 @@ import { getSession, loginWithCredentials, loginWithGoogle, register } from "../
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/auth/login:
+ *  post:
+ *      tags:
+ *          - Auth
+ *      summary: "Sign In"
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      required: ["email", "password"]
+ *                      properties: {
+ *                          email   : { type: "string" },
+ *                          password: { type: "string"}
+ *                      }
+ */
 router.post('/login',       loginWithCredentials);
 router.post('/register',    register);
 router.post('/google',      loginWithGoogle);
