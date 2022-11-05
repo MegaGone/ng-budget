a6df6723f8abd1fcb237adca00b700f0a3f03793
Javier Martínez <jmartinez@develsystems.com>
2022-11-05 12:37:58 -0600

    [UPDATE] Removed components module from app module and replaced start for flex-start navbar

--- BudgetApp.Frontend/src/app/app.module.ts
+++ BudgetApp.Frontend/src/app/app.module.ts
@@ -40,8 +40,7 @@ const routerConfig: ExtraOptions = {
         LayoutModule,
 
         // 3rd party modules that require global configuration via forRoot
-        MarkdownModule.forRoot({}),
-        ComponentsModule
+        MarkdownModule.forRoot({})
     ],
     bootstrap   : [
         AppComponent
