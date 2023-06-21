677b29eddd14539b6be19645f9bbb414b14865c4
J. Martinez <jmartinez@develsystems.com>
2023-06-21 17:40:01 -0600

    [UPDATE] Middlware fixed

--- BudgetApp.Backend/src/middlewares/global/error.middleware.ts
+++ BudgetApp.Backend/src/middlewares/global/error.middleware.ts
@@ -4,7 +4,5 @@ import { ResponseStatus } from "src/models";
 export const ErrorHandler = (error: ResponseStatus, req: Request, res: Response, next: NextFunction) => {
     const { statusCode = 500, message } = error;
     res.status(statusCode).send({ statusCode, message });
-
-    if (statusCode == 500) req.app.locals.logger.error(message);
     next();
 };
