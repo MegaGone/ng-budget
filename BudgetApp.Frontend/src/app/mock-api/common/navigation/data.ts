6767e6c24b92959080718793fa4f6bb9ee299da8
J. Martinez <jmartinez@develsystems.com>
2023-07-31 21:29:07 -0600

    [UPDATE] - Icons on navbar

--- BudgetApp.Frontend/src/app/mock-api/common/navigation/data.ts
+++ BudgetApp.Frontend/src/app/mock-api/common/navigation/data.ts
@@ -3,37 +3,73 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'dashboards',
+        title: 'Administración',
+        type: 'collapsable',
+        icon: 'mat_outline:verified_user',
+        children: [
+            {
+                id: 'dashboards.project',
+                title: 'Usuarios',
+                type: 'basic',
+                icon: 'mat_outline:supervised_user_circle',
+                link: '/dashboards/project'
+            },
+            {
+                id: 'dashboards.analytics',
+                title: 'Plantillas',
+                type: 'basic',
+                icon: 'heroicons_outline:template',
+                link: '/dashboards/analytics'
+            }
+        ]
+    },
+    {
+        id: 'example',
+        title: 'Gastos',
+        type: 'basic',
+        icon: 'mat_outline:add_shopping_cart',
+        link: '/example'
+    },
+    {
+        id: 'example',
+        title: 'Gastos fijos',
+        type: 'basic',
+        icon: 'mat_outline:push_pin',
+        link: '/example2'
+    },
+    {
+        id: 'example',
+        title: 'Reportes',
+        type: 'basic',
+        icon: 'heroicons_outline:document-report',
+        link: '/example3'
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
     }
 ];
