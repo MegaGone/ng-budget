c40fbed8116cb62a8e3811e16e3265defe9e95e4
J. Martinez <jmartinez@develsystems.com>
2023-08-03 20:00:14 -0600

    [UPDATE] - Remove unnecesary configuration on db config.

--- BudgetApp.Backend/src/config/database.config.ts
+++ BudgetApp.Backend/src/config/database.config.ts
@@ -1,13 +1,7 @@
 import "dotenv/config";
 
-const DB_HOSTNAME = process.env.DB_HOSTNAME  || "mongodb+srv://admin:*****.*****.mongodb.net/Admin";
-const DB_NAME = process.env.DB_NAME || "Sandbox";
-const DB_TLS = process.env.DB_TLS || true;
-const DB_SSL = process.env.DB_SSL || true;
+const DB_HOSTNAME = process.env.DB_HOSTNAME  || "mongodb+srv://admin:*****.*****.mongodb.net/ngBudget";
 
 export {
-    DB_HOSTNAME,
-    DB_NAME,
-    DB_SSL,
-    DB_TLS
+    DB_HOSTNAME
 };
